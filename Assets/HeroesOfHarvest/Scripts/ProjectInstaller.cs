03f9526879963d6f49ad3905a2e9e1f36eeea1ee
KarenKrill <vampart1997@gmail.com>
2025-11-02 00:54:45 +0500

    Added structured URP template project with TextMeshPro and Zenject

---
+++ Assets/HeroesOfHarvest/Scripts/ProjectInstaller.cs
@@ -0,0 +1,11 @@
+using Zenject;
+
+namespace HeroesOfHarvest
+{
+    public class ProjectInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+        }
+    }
+}
